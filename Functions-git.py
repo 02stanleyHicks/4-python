aa5d8e6cde27a427253f7921f1a2de7d0bebfb6c
Colton Hicks <02staneley.hicks@gulllakecs.org>
2019-02-14 12:21:49 -0500

    add the functions and variables

--- Functions-git.py
+++ Functions-git.py
@@ -1,8 +1,21 @@
-4.13.3: greetings
+#4.13.3: greetings
 #colton hicks
 #2.5.19
+'''
 name = input("what is your name: ")
 def greeting():
     print("Hi there " + name +"!")
     print ("nice to meet you ")
 greeting()
+'''
+#4.13.4 Functions and variables
+#Colton Hicks
+#2.11.19
+x=406
+def print_something():
+    x=3
+    print(x)
+print (x)
+print_something()
+
+
